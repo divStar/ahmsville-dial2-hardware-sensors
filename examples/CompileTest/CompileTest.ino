a7330b116de1e2e8066ff743e3d0da43af15fc78
Dr. Steven Strange <strange@my.family>
2023-10-02 00:23:59 +0200

    build: first time .gitlab-ci.yml builds

---
+++ examples/CompileTest/CompileTest.ino
@@ -0,0 +1,16 @@
+/**
+* @file    CompileTest.ino
+* @brief   Test program resembling a main file.
+*
+* This class is needed in order to test whether this library compiles.
+*
+* @author  Igor Voronin
+* @date    01.10.2023
+*/
+
+#include "AllRealSensors.h"
+#include "MacroKey.h"
+
+void setup() {}
+
+void loop() {}
