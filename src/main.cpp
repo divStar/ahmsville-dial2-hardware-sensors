5fce276790f0d7bd3059a874a68b24195db6d603
Dr. Steven Strange <strange@my.family>
2023-11-12 03:59:16 +0100

    feat(build): corrected metadata files and created a dummy main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+* @file   main.cpp
+* @brief  This file contains dummy setup() and loop() functions.
+*
+* @author Igor Voronin
+* @date   12.11.2023
+*/
+
+#include <Arduino.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
